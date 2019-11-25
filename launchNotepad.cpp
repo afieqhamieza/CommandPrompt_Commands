475fe69413933297faa584aaa2d4ef24b7f786f6
afieqhamieza <afieqhaazemi@gmail.com>
2019-11-24 21:13:22 -0500

    Launch notepad, obtain its PID, kill after 3 seconds

---
+++ launchNotepad.cpp
@@ -0,0 +1,19 @@
+#include <thread>        
+#include <chrono>
+using namespace std;
+
+
+
+int main()
+{
+	clock_t begin = clock();
+	system("start notepad.exe ");
+
+	system("tasklist /v /fi \"imagename EQ Notepad.exe\" /FO LIST | FIND \"PID:\"");
+
+    	std:this_thread::sleep_for(std::chrono::seconds(3));
+	clock_t end = clock();
+
+	system("Taskkill /IM Notepad.exe /F");
+
+}
